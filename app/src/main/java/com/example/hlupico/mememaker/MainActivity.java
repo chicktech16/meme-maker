1c2bd731e64ab5416cffd147edca389721aa02e5
chicktech16 <chicktechies+16@gmail.com>
2017-12-10 13:06:47 -0600

    my custom first step

--- app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
+++ app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.hlupico.mememaker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setElevation(0f);  /** Step 1 - remove automatic action bar shadow */
+        getSupportActionBar().setElevation(0f);
+
+        TextView getstarted = (TextView) findViewById(R.id.getstartedbutton);
+        getstarted.setTextColor(getResources().getColor(android.R.color.holo_blue_light));
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.new_meme_button);
+        View.OnClickListener imageClickListener = getImageButtonOnClick();
+        imageButton.setOnClickListener(imageClickListener);
+    }
+
+    private View.OnClickListener getImageButtonOnClick() {
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Y ou clicked this button", Toast.LENGTH_SHORT).show();
+                //Go to NewActivity
+                Intent intent = new Intent(MainActivity.this, NewMemeActivity.class);
+                startActivity(intent);
+
+            }
+        };
+
+        return clickListener;
     }
 }
+
+
+
+
